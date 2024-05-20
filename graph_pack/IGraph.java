a3dc7f72a9a9a5492fa2975a94f2785f8a60ae4f
W1LST3R <williampet02@gmail.com>
2024-05-20 18:58:47 +0200

    data och gränssnitett

---
+++ graph_pack/IGraph.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public interface IGraph
+  {
+  public void addVertex(String id, double population, double longitude, double latitude);
+  public void connectVertices(String id1, String id2, double weight);
+  public List<Vertex> getVertices();
+  public Vertex getStartVertex();
+  public void setTargetVertex(Vertex target);
+  public Vertex getTargetVertex();
+  public void clear(); // resets startVertex and targetVertex
+  public void findShortestPath(String start_id);
+  public void findShortestPath(Vertex start_vertex);
+  }
