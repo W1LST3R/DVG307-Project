55b12d03e1288d8380d0c808260f94c8ee3dcd6a
Spo0oKey <simon.muppen@hotmail.com>
2024-05-20 20:06:57 +0200

    l

---
+++ ProjektDVG307/src/dijkstrasAlgorithm/GraphTest.java
@@ -0,0 +1,16 @@
+package dijkstrasAlgorithm;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GraphTest {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+		Graph test = new Graph();
+		test.readVertexFile("H:\\git\\DVG307-Project\\ProjektDVG307\\src\\760_tatorter.csv");
+	}
+
+}
